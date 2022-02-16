b06fce824f6c12b93ca84f5a7582c6ff0876fe97
yourFriend <yourFriend@email.com>
2022-02-16 17:05:14 +0530

    Reached till Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
+  const Bang = 99 / 3;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Passing props from And displaying the parent components 🤳🤳
+  <>
+<Header title="Mjadi'sCafe"/>
+  </>
   );
 }
 
