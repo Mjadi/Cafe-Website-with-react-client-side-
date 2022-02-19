7e703ba0ec4a17395a9624ac656111f132ec6445
yourFriend <yourFriend@email.com>
2022-02-19 11:40:39 +0530

    Done with Cafe demo app

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ProtoTypes from 'prop-types'
 
+const onClick = () => {
+  console.log("Clicked");
+}
 export default function Header(props) {
   return (
     <header className="text-gray-600 body-font">
@@ -18,7 +21,7 @@ export default function Header(props) {
       <span className="ml-3 text-xl">{props.title}</span>
     </a>
     <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-      {props.button?<button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+      {props.button?<button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" onClick={onClick}>Button
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
